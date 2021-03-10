7496de81ee1c370ea1b3e07a1ce5181eb616a25e
Renan Dantas <dantas.renan@icloud.com>
2021-03-09 22:14:32 -0300

    adding starter_code

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
+import os
+
 from flask import Flask
+from flask_restful import Api
+
+from resources.item import Item, ItemList
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-  return {'message': 'Hello, world!'}
+app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+api = Api(app)
+
+api.add_resource(Item, '/item/<string:name>')
 
 if __name__ == '__main__':
-  app.run()
+    from db import db
+
+    db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
